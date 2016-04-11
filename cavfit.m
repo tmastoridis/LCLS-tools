9082a4f0455c4789042856ef1f5c68877796d5b3
Noah Sutton-Smolin <noahsutsmo@gmail.com>
2016-04-11 13:03:48 -0700

    Added power scaling to weight the peak of the function

--- cavfit.m
+++ cavfit.m
@@ -2,8 +2,9 @@ x_true = [-1e3, 1, 6e4];
 wrf = 2*pi*4e8;
 xdata = 2*pi*(-1e6:1e2:1e6);
 noisegain = 1e4;
+sclPow = 1.02;
 
-F = @(x,xdata)cavity_model(x,xdata,wrf);
+F = @(x,xdata)cavity_model(x,xdata,wrf).^sclPow;
 
 Hc = cavity_model(x_true, xdata, wrf);
 Hc_noise=Hc+noisegain*(randn(size(Hc))+1j*randn(size(Hc)));
@@ -15,9 +16,9 @@ x_fitted = lsqcurvefit(F, x_guess, xdata, Hc_noise);
 
 figure
 hold on
-plot(w/2/pi, 20*log10(Hc_noise))
-plot(w/2/pi, 20*log10(F(x_fitted, xdata)),'-g')
-plot(w/2/pi, 20*log10(Hc))
+plot(xdata/2/pi, 20*log10(Hc_noise))
+plot(xdata/2/pi, 20*log10(F(x_fitted, xdata)),'-g')
+plot(xdata/2/pi, 20*log10(Hc))
 
 %figure 
 %hold on
