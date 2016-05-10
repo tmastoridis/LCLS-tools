a7dbe9c38f613c945e3d24535c13f968282d6d3c
Noah Sutton-Smolin <noahsutsmo@gmail.com>
2016-05-10 05:44:12 -0700

    Attempting one-variable optimization on wo

--- cavfit.m
+++ cavfit.m
@@ -19,6 +19,29 @@ opts = optimset(opts, 'DiffMaxChange', 1e-5, ...
 % Turn 'off' 'iter' at final version (for display purposes only)
 opts = optimset(opts,'Display', 'off');
 
+QL_guess = 3e7;
+wo_guess = -1e4;
+
+cav_error_wrapper = @(x) cavity_error(sys_prm, [QL_guess, x], w, Hc_noise)
+wo_final = fminunc(cav_error_wrapper, wo_guess, opts)
+wo_true
+wo_error = (wo_final - wo_true) / wo_true
+
+QL_final = QL_guess % placeholder
+
+Hc_final = cavity_model(sys_prm, QL_final, wo_final, w);
+figure;
+plot(freq_range, Hc_noise, '-r', freq_range, Hc, '-b', freq_range, Hc_final, '-g');
+
+return
+
+% Optimization Options (number of iterations etc)
+opts = optimset('TolX', 1e-8, 'TolFun', 1e-8);
+opts = optimset(opts, 'DiffMaxChange', 1e-5, ...
+    'MaxFunEvals', 3e3, 'MaxIter', 1e3);
+% Turn 'off' 'iter' at final version (for display purposes only)
+opts = optimset(opts,'Display', 'off');
+
 x0 = [3e7 -1e4];
 
 % Use function fit_TF_pi to calculate the error function
