ded5bb5ae6cf27ba52c6ffe589b93859e5d7babb
Noah Sutton-Smolin <noahsutsmo@gmail.com>
2016-05-14 02:02:05 -0700

    Switched to log plot; switched to multiplicative noise

--- cavfit.m
+++ cavfit.m
@@ -4,13 +4,13 @@ sys_prm = model_init();
 freq_range = -2e4:0.25:2e4;
 w = 2*pi*freq_range;
 
-noisegain = 1.5e9;
+noisegain = 0.05;
 
 wo_true = - 1e3 * 2 * pi;
 QL_true = 4.12e7;
 
 Hc = cavity_model(sys_prm, QL_true, wo_true, w);
-Hc_noise=Hc+noisegain*(randn(size(Hc))+1j*randn(size(Hc)));
+Hc_noise=Hc.*(1+noisegain*(randn(size(Hc))+1j*randn(size(Hc))));
 
 %% FIRST STEP - wo optimization
 
@@ -31,7 +31,7 @@ wo_error = (wo_final - wo_true) / wo_true
 
 Hc_final = cavity_model(sys_prm, QL_guess, wo_final, w);
 figure;
-plot(freq_range, Hc_noise, '-r', freq_range, Hc, '-b', freq_range, Hc_final, '-g');
+plot(freq_range, 20*log10(Hc_noise), '-r', freq_range, 20*log10(Hc), '-b', freq_range, 20*log10(Hc_final), '-g');
 
 return
 %% SECOND STEP - QL optimization
@@ -52,4 +52,4 @@ QL_error = (QL_final - QL_true) / QL_true
 
 Hc_final = cavity_model(sys_prm, QL_final, wo_final, w);
 figure;
-plot(freq_range, Hc_noise, '-r', freq_range, Hc, '-b', freq_range, Hc_final, '-g');
+plot(freq_range, 20*lot10(Hc_noise), '-r', freq_range, 20*log10(Hc), '-b', freq_range, 20*log10(Hc_final), '-g');
